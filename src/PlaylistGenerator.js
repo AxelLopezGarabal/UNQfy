09410c91d50b337ce0b911fa5e776f7191f2fb53
David Gonzalez <davidgonzalezjscosasunq@gmail.com>
2019-09-11 21:16:27 -0300

    Refactor en PlaylistGenerator

--- src/PlaylistGenerator.js
+++ src/PlaylistGenerator.js
@@ -3,19 +3,21 @@ const Playlist = require('./entities/Playlist')
 class PlaylistGenerator {
 
   generate(id, playlistName, genresToInclude, maxDuration, tracksToChooseFrom) {
-    return tracksToChooseFrom.reduce((newPlaylist, aTrack) =>
-      aTrack.matchSomeGenreFrom(genresToInclude) && this._doesNotExcedeMaxDurationAfterAdding(aTrack, newPlaylist, maxDuration)
-        ? newPlaylist.addTrack(aTrack)
-        : newPlaylist,
-      this._createEmptyPlaylist(id, playlistName)
-    )
+    return tracksToChooseFrom
+      .filter(track => track.matchSomeGenreFrom(genresToInclude))
+      .reduce((newPlaylist, aTrack) =>
+          this._doesNotExcedeMaxDurationAfterAddingTo(newPlaylist, aTrack, maxDuration)
+          ? newPlaylist.addTrack(aTrack)
+          : newPlaylist,
+        this._createEmptyPlaylist(id, playlistName)
+      )
   }
 
   _createEmptyPlaylist(id, name) {
     return new Playlist({ id, name })
   }
 
-  _doesNotExcedeMaxDurationAfterAdding(aTrack, aPlaylist, aDuration) {
+  _doesNotExcedeMaxDurationAfterAddingTo(aPlaylist, aTrack, aDuration) {
     return aTrack.duration + aPlaylist.duration() <= aDuration
   }
 
