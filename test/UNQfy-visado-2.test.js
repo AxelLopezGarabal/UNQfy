fa67c2e6b57b6f55733e183b7625425afa5501d5
David Gonzalez <davidgonzalezjscosasunq@gmail.com>
2019-10-17 19:58:12 -0300

    Refactors en tests

--- test/UNQfy-visado-2.test.js
+++ test/UNQfy-visado-2.test.js
@@ -1,40 +1,11 @@
 const { expect } = require('chai')
-const { UNQfy }  = require('../model/unqfy')
 
-// TODO: extraer helpers a un archivo
-const makeUNQfy = () => new UNQfy()
-
-const makeArtistData = () => ({name: 'pepe', country: 'argentina'})
-const makeAlbumData  = name => ({ name })
-const makeTrackData  = () => ({ })
-
-function createAndAddArtist(unqfy, artistName, country) {
-    const artist = unqfy.addArtist({ name: artistName, country })
-    return artist
-}
-  
-function createAndAddAlbum(unqfy, artistId, albumName, albumYear) {
-    return unqfy.addAlbum(artistId, { name: albumName, year: albumYear })
-}
-  
-function createAndAddTrack(unqfy, albumName, trackName, trackDuraction, trackGenres) {
-    return unqfy.addTrack(albumName, { name: trackName, duration: trackDuraction, genres: trackGenres })
-}
+const {
+    makeUNQfy,
+    makeUNQfyWithArtistWithAlbums
+} = require('./helpers/factories')
 
 describe('UNQfy', () => {
-    let unqfy
- 
-    beforeEach(() => {
-        unqfy        = makeUNQfy()
-        artist       = unqfy.addArtist(makeArtistData())
-        album01      = unqfy.addAlbum(artist.id, makeAlbumData('album01'))
-        trackAlbum01 = unqfy.addTrack(album01.id, {name: 't1', duration: 100, genres: ['punk']})
-        
-        album02      = unqfy.addAlbum(artist.id, makeAlbumData('album02'))
-        trackAlbum02 = unqfy.addTrack(album02.id, {name: 't2', duration: 100, genres: ['punk']})
-
-        playlist     = unqfy.createPlaylist('name', ['punk'], 1000000)
-    })
 
     describe('Se le pueden pedir los albumes de un artista en base a su nombre', () => {
         describe('... si el artista no existe arroja una excepcion', () =>{
@@ -47,7 +18,7 @@ describe('UNQfy', () => {
         })
 
         describe('... si el artista existe retorna todos sus albumes', () =>{
-            const {unqfy, artist, albums} = makeUNQfyWithArtist(makeArtistData(), [makeAlbumData()])
+            const {unqfy, artist, albums} = makeUNQfyWithArtistWithAlbums()
 
             expectHasOnly(
                 albums,
@@ -56,18 +27,6 @@ describe('UNQfy', () => {
     })
         
 })
-
-function makeUNQfyWithArtist(artistData, albumsData) {
-    const unqfy  = makeUNQfy()
-    const artist = unqfy.addArtist(artistData)    
-    albumsData.forEach(albumData => unqfy.addAlbum(artist.id, albumData))
-    
-    return {
-        unqfy,
-        artist,
-        albums: artist.albums
-    }
-}
 
 function expectHasOnly(expectedElements, aCollection) {
     expect(aCollection).to.have.lengthOf(expectedElements.length)
---
+++ test/helpers/factories.js
@@ -0,0 +1,46 @@
+const { UNQfy }  = require('../../model/unqfy')
+
+const makeUNQfy      = ()   => new UNQfy()
+
+const makeArtistData = ()   => ({name: 'pepe', country: 'argentina'})
+const makeAlbumData  = name => ({ name })
+const makeTrackData  = ()   => ({ })
+  
+const createAndAddArtist = (unqfy, artistName, country) =>
+  unqfy.addArtist({ name: artistName, country })
+    
+const createAndAddAlbum = (unqfy, artistId, albumName, albumYear) =>
+  unqfy.addAlbum(artistId, { name: albumName, year: albumYear })
+    
+const createAndAddTrack = (unqfy, albumName, trackName, trackDuraction, trackGenres) =>
+  unqfy.addTrack(albumName, { name: trackName, duration: trackDuraction, genres: trackGenres })
+  
+const makeUNQfyWithArtist = (artistData, albumsData) => {
+  const unqfy  = makeUNQfy()
+  const artist = unqfy.addArtist(artistData)    
+  
+  albumsData.forEach(albumData => unqfy.addAlbum(artist.id, albumData))
+  
+  return {
+      unqfy,
+      artist,
+      albums: artist.albums
+  }
+}
+
+const makeUNQfyWithArtistWithAlbums = () =>
+  makeUNQfyWithArtist(makeArtistData(), [makeAlbumData()])
+
+module.exports = {
+
+  makeUNQfy,
+  makeArtistData,
+  makeAlbumData,
+  makeTrackData,
+  createAndAddArtist,
+  createAndAddAlbum,
+  createAndAddTrack,
+  makeUNQfyWithArtist,
+  makeUNQfyWithArtistWithAlbums
+
+}
