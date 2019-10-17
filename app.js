4c6637b481eeae44674aacce5eaf0c3f4c922178
davidgonzalezjscosasunq@gmail.com <davidgonzalezjscosasunq@gmail.com>
2019-10-17 16:32:34 -0300

    Method chaining en app

--- app.js
+++ app.js
@@ -5,27 +5,27 @@ const bodyParser = require('body-parser');
 
 const ticketRoutes = require('./routes/default');
 
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app
+	.use(morgan('dev'))
+	.use(bodyParser.urlencoded({extended: true}))
+	.use(bodyParser.json())
 
-app.use('/', ticketRoutes);
+	.use('/', ticketRoutes)
 
 
-
-app.use((req, res, next)=>{
-	const error = new Error('Not found');
-	error.status = 404;
-	next(error);
-});
-
-app.use((error, req, res, next) => {
-	res.status(error.status || 500);
-	res.json({
-		error:{
-			message: error.message
-		}
+	.use((req, res, next) => {
+		const error = new Error('Not found')
+		error.status = 404
+		next(error)
 	})
-});
+
+	.use((error, req, res, next) => {
+		res.status(error.status || 500);
+		res.json({
+			error:{
+				message: error.message
+			}
+		})
+})
 
 module.exports = app; 
