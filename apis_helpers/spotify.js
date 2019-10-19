5558bfe38cc6716f022ae4139cd888aaa68c0edd
David Gonzalez <davidgonzalezjscosasunq@gmail.com>
2019-10-19 11:29:44 -0300

    spotify populatedAlbumsDataFor

--- apis_helpers/spotify.js
+++ apis_helpers/spotify.js
@@ -1,12 +1,12 @@
 const rp = require('request-promise');
 
-const accessToken = 'BQDf0rIp1qVTKMaCIf27X8w8rGutZ0z90aspbckzFZdtbBcPLAFLlr_MOGI3jUP-aAzqD7MDq6O_6WhMt7mZ6I1YdSc2JGtvarZa18UK1lhoAU518A64lWA_vyBGwy34WuPE3_Jzim7ojcwqMTmFC9HjkeYSlGRXv5L9zbAagTD5uaJ6fuax'
+const accessToken = 'BQBNPBeMttO7wiWzn1n70qQLNUPwMyQE3haHmhlpbg5WE2ck8umlrbXyl2dOC3DbymVNJzUt7xr9md5Nz3N4c5CkXWI67dIIaSxceiJOSbVSvbnf-rNVX_KCHmbtwj-oGlgnv9958b8h5NsWeVTqzQEaa8qN3pj7lkvKqEYQf-O-_1OtKQqC'
 
 const baseURL = 'https://api.spotify.com/v1'
 
-const get = resourseURL => query =>
+const get = path => query =>
 	rp.get({
-		uri: baseURL + resourseURL,
+		uri: baseURL + path,
 		headers: { Authorization: 'Bearer ' + accessToken },
 		qs: query,
 		json: true
@@ -47,14 +47,17 @@ const extractTrackData = spotifyTrack =>({
 	duration: spotifyTrack.duration_ms
 })
 
+const populatedAlbumsDataFor = artistName =>
+	allAlbums(artistName)
+		.then(albums => albums.map(extractAlbumData))
 
-allAlbums('beatles')
-	.then(albums => albums.map(extractAlbumData))
-
+// Prueba
+populatedAlbumsDataFor('beatles')
 	.then(albums => JSON.stringify(albums, null, 2))
 	.then(console.log)
 
 module.exports = {
+	populatedAlbumsDataFor,
 	findArtist,
 	allAlbums
 }
