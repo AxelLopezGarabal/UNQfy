7eab63a749c3f9414a2aaccb0204b9d0521d5520
David Gonzalez <davidgonzalezjscosasunq@gmail.com>
2019-10-20 18:50:50 -0300

    Mas tests de la api

--- test/api/coso.test.js
+++ test/api/coso.test.js
@@ -8,7 +8,16 @@ chai.use(chaiHttp)
 
 // OJO: si la app arranca con cosas cargadas puede fallar algun test
 
+const resourceAlreayExistResponseBody = { errorCode: 'RESOURCE_ALREADY_EXIST' }
+const resourceNotFoundResponseBody    = { errorCode: "RESOURCE_NOT_FOUND" }
+
+
 const artistsEndPoint = '/api/artists'
+
+const artistData = {
+  name: 'pepe',
+  country: 'argentina'
+}
 
 describe(artistsEndPoint, () => {
   let app
@@ -24,66 +33,73 @@ describe(artistsEndPoint, () => {
 
   describe('POST', () => {
     it('crea un nuevo artista', done => {
-      const requestBody = {
-        name: 'pepe',
-        country: 'argentina'
-      }
-
       const responseBody = {
         id: 0,
         albums: [],
-        ...requestBody
+        ...artistData
       }
 
       chai.request(app)
         .post(artistsEndPoint)
-        .send(requestBody)
-        .end((err, res) => {
-          expect(res).to.have.status(201)
-          expect(res).to.be.json
-          expect(res.body).to.eql(responseBody)
+        .send(artistData)
+        .end((error, res) => {
+          expectResponse(res, 201, responseBody)
           done()
         })
     })
 
     it('no puede haber mas de un artista con el mismo nombre', done => {
-      const requestBody = {
-        name: 'pepe',
-        country: 'argentina'
-      }
-
-      const resourceAlreayExistResponseBody = { errorCode: 'RESOURCE_ALREADY_EXIST' }
-
       chai.request(app)
         .post(artistsEndPoint)
-        .send(requestBody)
+        .send(artistData)
         .end(() => {
 
           chai.request(app)
             .post(artistsEndPoint)
-            .send(requestBody)
-            .end((err, res) => {
-              expect(res).to.have.status(409)
-              expect(res).to.be.json
-              expect(res.body).to.eql(resourceAlreayExistResponseBody)
+            .send(artistData)
+            .end((error, res) => {
+              expectResponse(res, 409, resourceAlreayExistResponseBody)
               done()
             })
 
         })
       
-      
     })
   })
 
-  // describe('GET ONE', () => {
-  //   it('DASDASD', done => {
-  //     chai.request(app)
-  //       .get(artistsEndPoint + '/123456')
-  //       .end((err, res) => {
-  //         expect(res).to.have.status(404)
-  //         done()
-  //       })
-  //   })
-  // })
+  describe('GET ONE', () => {
+    it('error cuando no existe el artista', done => {
+      chai.request(app)
+        .get(artistsEndPoint + '/0')
+        .end((error, res) => {
+          expectResponse(res, 404, resourceNotFoundResponseBody)
+          done()
+        })
+    })
+
+    it('exito dasdasdas', done => {
+      chai.request(app)
+        .post(artistsEndPoint)
+        .send(artistData)
+        .end(() => {
+
+          chai.request(app)
+            .get(artistsEndPoint + '/0')
+            .end((error, res) => {
+              expectResponse(res, 200, { id: 0, albums: [],  ...artistData})
+              done()
+            })
+
+
+        })
+
+    })
+  })
 
 })
+
+function expectResponse(res, statusCode, responseBody) {
+  expect(res).to.be.json
+  expect(res).to.have.status(statusCode)
+  expect(res.body).to.eql(responseBody)
+}
