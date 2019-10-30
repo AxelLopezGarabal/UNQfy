440b7cc71be6a1fa3e619f4715c38edffb52cfb2
AxelLopezGarabal <axel.lopez.garabal@gmail.com>
2019-10-30 14:05:01 -0300

    mod por el errorhandler

--- routes/artist.js
+++ routes/artist.js
@@ -1,3 +1,6 @@
+const badRequestErrorModule = require('../errors/BadRequestError')
+const resourceAlreadyExistErrorModule = require('../errors/ResourceAlreadyExistError')
+const resourceNotFoundErrorModule = require('../errors/ResourceNotFoundError')
 
 const express = require('express');
 const router = express.Router();
@@ -12,25 +15,14 @@ router.post('/', (req, res, next) => {
 	if( c1 && c2 && c3){
 		if(! system.existsArtistWithName(body.name)){
 			const newartist = system.addArtist(body)
-			res.status(201).json({
-				id: newartist.id,
-				name: newartist.name,
-				albums: newartist.albums,
-				country: newartist.country
-			});
+			res.status(201).json(newartist);
 		}
 		else{
-			res.status(409).json({
-				status: 409,
-				errorCode: "RESOURCE_ALREADY_EXISTS"
-			});
+			next(new resourceAlreadyExistErrorModule.ResourceAlreadyExistError())
 		}	
 	}
 	else{
-		res.status(400).json({
-			status: 400,
-			errorCode: 'BAD_REQUEST'
-		});
+		next(new badRequestErrorModule.BadRequestError())
 	}
 });
 
@@ -38,15 +30,10 @@ router.get('/:id', (req, res, next) => {
 	const idParam = parseInt(req.params.id);
 	if(system.verifyId(idParam) && idParam > 0){
 		const artist = system.getArtistById(idParam);
-		res.status(200).json(
-			artist
-		)
+		res.status(200).json(artist)
 	}
 	else{
-		res.status(404).json({
-			status: 404,
-			errorCode: 'RESOURCE_NOT_FOUND'
-		})
+		next(new resourceNotFoundErrorModule.ResourceNotFoundError())
 	}
 });
 
@@ -55,18 +42,10 @@ router.put('/:id', (req, res, next) => {
 	const body = req.body
 	if(system.verifyId(idParam) && idParam > 0){
 		const modArtist = system.updateArtist(idParam, body)
-		res.status(200).json({
-			id: modArtist.id,
-			name: modArtist.name,
-			albums: modArtist.albums,
-			country: modArtist.country
-		})
+		res.status(200).json(modArtist)
 	}
 	else{
-		res.status(404).json({
-			status: 404,
-			errorCode: 'RESOURCE_NOT_FOUND'
-		})
+		next(new resourceNotFoundErrorModule.ResourceNotFoundError())
 	}
 });
 
@@ -92,10 +71,7 @@ router.delete('/:id', (req, res, next) => {
 		})
 	}
 	else{
-		res.status(404).json({
-			status: 404,
-			errorCode: 'RESOURCE_NOT_FOUND'
-		})
+		next(new resourceNotFoundErrorModule.ResourceNotFoundError())
 	}
 });
 
