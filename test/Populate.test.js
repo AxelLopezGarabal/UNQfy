5ce781550fab465493d584f7bd1887441256c5c7
AxelLopezGarabal <axel.lopez.garabal@gmail.com>
2019-11-05 00:05:58 -0300

    se persiste las respuestas de las request para verificar que funcionan

--- test/Populate.test.js
+++ test/Populate.test.js
@@ -4,15 +4,27 @@ const lyrpro = require('../musicMatch').module.LyricFinder
 const UNQfymodule = require('../model/unqfy')
 const { Artist, Album} = require('../model/entities/all.js')
 
-//const unqfy = new UNQfymodule.UNQfy()
-//unqfy.addArtist({name:'Gorillaz',country:'USA'})
-//console.log(unqfy.getAlbumsForArtist('Gorillaz'))
-//unqfy.addAlbum(1,{name:'popo', year:'2020'})
+const unqfy = new UNQfymodule.UNQfy();
 
-const lf = new lyrpro();
-let lyrics = lf.getLyrics('humility', 'Gorillaz');
-console.log(lyrics);
-console.log(lyrics);
-console.log(lyrics);
-console.log(lyrics);
+function wait(ms){
+   var start = new Date().getTime();
+   var end = start;
+   while(end < start + ms) {
+     end = new Date().getTime();
+  }
+}
 
+
+unqfy.addArtist({name:'Gorillaz',country:'USA'})
+
+unqfy.getAlbumsForArtist('Gorillaz')
+
+wait(7000);
+
+//let unq = unqfy.load('backend.json')
+unqfy.addAlbum(1,{name:'popo', year:'2020'})
+unqfy.addTrack(2,{name:'humility', duration: 4000, genres:[]})
+unqfy.getLyiricsFor('Gorillaz', 'humility')
+
+
+console.log(unqfy.getArtistByName('Gorillaz').album)
