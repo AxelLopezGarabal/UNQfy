2d0557a0325b743cda7581542ad76983f4fa375a
AxelLopezGarabal <axel.lopez.garabal@gmail.com>
2019-12-11 00:52:04 -0300

    se modifica el nombre de server a serverNotify

--- api.js
+++ api.js
@@ -1,12 +1,12 @@
 const axios = require('axios')
 
-const server = 'http://172.20.0.22:5001/api';
+const serverNotify = 'http://172.20.0.22:5001/api';
 const serverLog = 'http://172.20.0.23:5002/api/'
 
 const API ={
-    get: path => axios.get(`${server}${path}`).then(response => response),
-    post: (path, body) => axios.post(`${server}${path}`, body).then(res => res),
+    get: path => axios.get(`${serverNotify}${path}`).then(response => response),
+    post: (path, body) => axios.post(`${serverNotify}${path}`, body).then(res => res),
     postLog: (body) => axios.post(`${serverLog}`, body).then(res => res),
-    put: (path, body) => axios.put(`${server}${path}`, body).then(console.log(body))
+    put: (path, body) => axios.put(`${serverNotify}${path}`, body).then(console.log(body))
 }
 module.exports = API;
